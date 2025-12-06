3e6b99a1a1700e44570b328093c140c84051b3c0
YknotTYD <ethan.bertin-prevot@gmail.com>
2025-12-06 17:33:35 +0100

    [main] Made it so that curl.sh installs starship if necessary - 'pass'.

--- curl.sh
+++ curl.sh
@@ -3,6 +3,12 @@
 
 set -eu
 
+#check to add eval thing to bashrc
+
+if ! command -v starship; then
+	curl -sS https://starship.rs/install.sh | sh -s -- --version v1.24.1
+fi
+
 TEMP_DIR=$(mktemp -d)
 cd "$TEMP_DIR"
 trap 'rm -rf "$TEMP_DIR"' EXIT
