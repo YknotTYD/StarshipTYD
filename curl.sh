333ba8d54126282f72f04cee0ea51a6311340258
YknotTYD <>
2025-12-29 20:59:31 +0100

    [main] Improved curl.sh and made the starship installation no-sudo - 'pass'.

--- curl.sh
+++ curl.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 #curl.sh
 
+# installs starship if necessary
+# creates a temporary directory to work in
+# clones the repo
+# launches starship-tyd-install.sh
+
 if [ -z "$HOME" ]; then
 	echo "\$HOME is unset." 1>&2
 	exit 1
@@ -11,20 +16,31 @@ set -eu
 BASHRC="$HOME/.bashrc"
 touch "$BASHRC"
 
-if ! command -v starship > /dev/null; then
-	curl -sSLf https://starship.rs/install.sh | sh -s -- --version v1.24.1
+if ! command -v starship > /dev/null 2>&1; then # starship: command not found
+
+	mkdir -p "$HOME/.local/bin"             # add ~/.local/bin to PATH
+	SETPATH='PATH="$HOME/.local/bin:$PATH"' #
+                                            #
+	if ! grep -q "$SETPATH" "$BASHRC"; then #
+		echo ''         >> "$BASHRC"        #
+		echo "$SETPATH" >> "$BASHRC"        #
+	fi                                      #
+
+	curl -sSLf https://starship.rs/install.sh | sh -s -- --version v1.24.1 --bin-dir "$HOME/.local/bin" # install starship
 fi
 
-if ! grep -q "starship init bash" "$BASHRC"; then
-	echo '' >> "$BASHRC"
-	echo 'eval "$(starship init bash)"' >> "$BASHRC"
+STARSHIP_INIT='eval "$(starship init bash)"'  # add starship to ~/.bashrc
+                                              #
+if ! grep -q "$STARSHIP_INIT" "$BASHRC"; then #
+	echo ''               >> "$BASHRC"        #
+	echo "$STARSHIP_INIT" >> "$BASHRC"        #
 fi
 
 TEMP_DIR=$(mktemp -d)
-cd "$TEMP_DIR"
 trap 'rm -rf "$TEMP_DIR"' EXIT
+cd "$TEMP_DIR"
 
 git clone https://github.com/YknotTYD/StarshipTYD.git
 cd StarshipTYD
 
-sh "starship-tyd-install.sh"
+sh "./starship-tyd-install.sh"
